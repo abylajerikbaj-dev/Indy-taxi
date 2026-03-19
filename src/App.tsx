938343f80a4c04476319b8a895411f27cdfe78b6
abylajerikbaj-dev <abylajerikbaj@gmail.com>
2026-03-19 23:10:52 +0500

    Create src/App.tsx file with App component implementation

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import ChatWindow from './ChatWindow';
+
+const App = () => {
+    return (
+        <div>
+            <Sidebar />
+            <ChatWindow />
+        </div>
+    );
+};
+
+export default App;
